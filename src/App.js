814b3e58f9577922dbf27229526c9a34821b324e
thanhnguyennguyen <nguyenbk92@gmail.com>
2018-08-06 01:17:59 +0700

    front-end lottery app

--- src/App.js
+++ src/App.js
@@ -2,39 +2,86 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import web3 from './web3';
-import lottery from '/.lottery';
+import lottery from './lottery';
+
+const etherUnit = 'ether';
+let manager = '';
+
 
 class App extends Component {
     state = {
         manager: '',
         players: [],
         balance: '',
-        value: ''
+        value: '',
+        message: ''
     }
     async componentDidMount() {
-        const manager = await lottery.methods.manager.call();
-        const players = await lottery.methods.getPlayers.call();
+        manager = await lottery.methods.manager().call();
+        const players = await lottery.methods.getPlayers().call();
         const balance = await web3.eth.getBalance(lottery.options.address);
         this.setState({
-            manager,
-            players,
-            balance,
-            value: ''
+            manager: manager,
+            players: players,
+            balance: balance,
+            value: '',
+            message: ''
         });
     }
 
     onSubmit = async (event) => {
         event.preventDefault();
-        await lottery.methods.enter().call();
+        const accounts = await web3.eth.getAccounts();
+        this.setState({message: 'Waiting on transaction success...'});
+        try {
+            await lottery.methods.enter().send({
+                from: accounts[0],
+                value: web3.utils.toWei(this.state.value, etherUnit)
+            });
+            const players = await lottery.methods.getPlayers.call();
+            const balance = await web3.eth.getBalance(lottery.options.address);
+            this.setState({
+                manager: manager,
+                players: players,
+                balance: balance,
+                value: '',
+                message: 'You have been entered!'
+            });
+        } catch (error) {
+            this.setState({message: 'The transaction couldnot complete successfully'});
+            console.log(error);
+        }   
     }
+
+    onClick = async () => {
+        const accounts = await web3.eth.getAccounts();
+        this.setState({message: 'Waiting on transaction success...'});
+        try {
+            await lottery.methods.pickWinner().send({
+                from: accounts[0]
+            });
+            this.setState({
+                manager: manager,
+                players: [],
+                balance: '0',
+                value: '',
+                message: 'A winner has been picked!'
+            });
+        } catch (error) {
+            this.setState({message: 'The transaction couldnot complete successfully'});
+            console.log(error);
+        }
+        
+    }
+
     render() {
         return (
             <div className="App">
                 <h2>Lottery contract</h2>
                 <p>This contract is managed by {this.state.manager}.</p>
                 <p>There are currently {this.state.players.length} people (person)
-         entered competing to win {web3.utils.fromWei(this.state.manager.balance, ether)} ether</p>
-
+         entered competing to win {web3.utils.fromWei(this.state.balance, etherUnit)} ether</p>
+                <hr/>
                 <form onSubmit={this.onSubmit}>
                     <h4>Want to try your luck ?</h4>
                     <div>
@@ -45,6 +92,11 @@ class App extends Component {
                     </div>
                     <button>Sign me up!</button>
                 </form>
+                <hr/>
+                <h4>{this.state.message}</h4>
+                <hr/>
+                <h4>Let's pick a winner</h4>
+                <button onClick={this.onClick}>Pick a Winner</button>
             </div>
         );
     }
